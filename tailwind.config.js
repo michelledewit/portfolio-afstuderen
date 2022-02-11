82d63f7df858efa2f0ec24e732a7b6407a6272df
Michelle de Wit <michelle@MacBook-Pro-van-Brik.local>
2022-02-11 16:53:47 +0100

    add set-up

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  purge: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#CEE8DC',
+        secondary: {
+          100: '#E2E2D5',
+          200: '#888883',
+        }
+      },
+      fontSize:{
+
+      }
+      ,
+    },
+  },
+  variants: {},
+  plugins: [],
+}
