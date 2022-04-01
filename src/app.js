ed897afdb5d17677b5a0a2102aa6c15059888a12
Michelle de Wit <michelle@MacBook-Pro-van-Brik.local>
2022-04-01 10:37:05 +0200

    glightbox functie toegevoegd

--- src/app.js
+++ src/app.js
@@ -1,5 +1,12 @@
 import "tailwindcss/tailwind.css";
+import GLightbox from 'glightbox';
 
 <script src="node_modules/@glidejs/glide/dist/glide.min.js"></script>
 
   new Glide('.glide').mount()
+
+  const lightbox = GLightbox({
+    touchNavigation: true,
+    loop: true,
+});
+
